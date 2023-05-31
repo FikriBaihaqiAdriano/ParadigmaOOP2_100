28cbf70bb88c88de503075e96b688446e1ebdd6a
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-05-31 17:57:01 +0700

    membuat class derivedClass dari turunan baseClass dengan akses modifier public

--- noOverridding/noOverridding.cpp
+++ noOverridding/noOverridding.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 class baseClass {
 public:
+	virtual void perkenalan() final {
+		cout << "Hallo saya Function dari base class";
+	}
+};
+
+class derivedClass : public baseClass {
+public:
 
 };
