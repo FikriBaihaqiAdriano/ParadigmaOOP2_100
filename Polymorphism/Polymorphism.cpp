99a7cf273a2b4df8ae521e9021dcf3f0e7f27bc9
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-05-31 17:42:57 +0700

    membuat class joko dari turunan seseorang dengan akses modifier

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 class seseorang {
 public:
+	virtual void pesan() = 0;
+	//virtual void pesan(){
+		//cout << "Pesan dari seseorang" << endl;
+	//}
+	//virtual void cetak() = 0;
+	virtual void cetak(){
+
+	}
+};
+
+class joko : public seseorang {
+public:
 
 };
